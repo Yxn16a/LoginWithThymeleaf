60e0173068ee7f089d118cc1c564bdf311082187
Yves <yxn16a@acu.edu>
2022-05-18 03:11:01 +0200

    Initial commit

---
+++ src/main/java/com/example/loginwiththymeleaf/LoginWithThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginwiththymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginWithThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loginwiththymeleaf/LoginWithThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginwiththymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
